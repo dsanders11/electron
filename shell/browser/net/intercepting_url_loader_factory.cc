c66e3cfe4387fd57b90b314e88bc51cea5014577
David Sanders <dsanders11@ucsbalum.com>
2021-06-10 06:48:48 -0700

    wip: support for redirects

--- shell/browser/net/intercepting_url_loader_factory.cc
+++ shell/browser/net/intercepting_url_loader_factory.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 GitHub, Inc.
+// Copyright (c) 2021 GitHub, Inc.
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
@@ -14,35 +14,228 @@
 #include "net/base/completion_repeating_callback.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_util.h"
+#include "net/url_request/redirect_util.h"
 #include "services/network/public/cpp/features.h"
 #include "shell/common/options_switches.h"
 
 namespace electron {
 
+InterceptingURLLoaderFactory::InterceptedRequest::FollowRedirectParams::
+    FollowRedirectParams() = default;
+InterceptingURLLoaderFactory::InterceptedRequest::FollowRedirectParams::
+    ~FollowRedirectParams() = default;
+
 InterceptingURLLoaderFactory::InterceptedRequest::InterceptedRequest(
-    mojo::PendingReceiver<network::mojom::URLLoader> loader,
+    const InterceptHandlersMap& intercepted_handlers,
+    mojo::PendingReceiver<network::mojom::URLLoader> loader_receiver,
     int32_t request_id,
     uint32_t options,
     const network::ResourceRequest& request,
     mojo::PendingRemote<network::mojom::URLLoaderClient> client,
     const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
-    ProtocolType type,
-    mojo::PendingRemote<network::mojom::URLLoaderFactory> interceptor_factory,
-    mojo::PendingRemote<network::mojom::URLLoaderFactory> target_factory)
-    : loader_(std::move(loader)),
+    mojo::PendingRemote<network::mojom::URLLoaderFactory> target_factory_remote)
+    : intercepted_handlers_(intercepted_handlers),
       request_id_(request_id),
       options_(options),
       request_(request),
-      client_(std::move(client)),
       traffic_annotation_(traffic_annotation),
-      type_(type),
-      interceptor_factory_(std::move(interceptor_factory)),
-      target_factory_(std::move(target_factory)) {}
+      client_(std::move(client)),
+      target_factory_remote_(std::move(target_factory_remote)) {
+  loader_receiver_.Bind(std::move(loader_receiver));
+  loader_receiver_.set_disconnect_handler(base::BindOnce(
+      &InterceptingURLLoaderFactory::InterceptedRequest::OnLoaderDisconnect,
+      base::Unretained(this)));
+  client_.set_disconnect_handler(base::BindOnce(
+      &InterceptingURLLoaderFactory::InterceptedRequest::OnClientDisconnect,
+      base::Unretained(this)));
+
+  // TODO - Set disconnect handlers for the dynamically bound stuff - actually
+  // this needs to be done on each binding
+
+  MaybeRunInterceptHandler();
+}
 
 InterceptingURLLoaderFactory::InterceptedRequest::~InterceptedRequest() =
     default;
 
+void InterceptingURLLoaderFactory::InterceptedRequest::FollowRedirect(
+    const std::vector<std::string>& removed_headers,
+    const net::HttpRequestHeaders& modified_headers,
+    const net::HttpRequestHeaders& modified_cors_exempt_headers,
+    const absl::optional<GURL>& new_url) {
+  // Save the follow redirect params, if the scheme is intercepted then
+  // the handler needs to be run before deciding whether to continue.
+  // References code in
+  // WebRequestProxyingURLLoaderFactory::InProgressRequest::FollowRedirect
+  auto params = std::make_unique<FollowRedirectParams>();
+  params->removed_headers = removed_headers;
+  params->modified_headers = modified_headers;
+  params->modified_cors_exempt_headers = modified_cors_exempt_headers;
+  params->new_url = new_url;
+  pending_follow_redirect_params_ = std::move(params);
+
+  // Update |request_| with info from the redirect, so that it's accurate
+  // when provided to a new loader if not following redirect with original
+  // The following references code in WorkerScriptLoader::FollowRedirect
+  DCHECK(redirect_info_);
+
+  bool should_clear_upload = false;
+  net::RedirectUtil::UpdateHttpRequest(
+      request_.url, request_.method, *redirect_info_, removed_headers,
+      modified_headers, &request_.headers, &should_clear_upload);
+  request_.cors_exempt_headers.MergeFrom(modified_cors_exempt_headers);
+  for (const std::string& name : removed_headers)
+    request_.cors_exempt_headers.RemoveHeader(name);
+
+  if (should_clear_upload)
+    request_.request_body = nullptr;
+
+  request_.url = redirect_info_->new_url;
+  request_.method = redirect_info_->new_method;
+  request_.site_for_cookies = redirect_info_->new_site_for_cookies;
+  request_.referrer = GURL(redirect_info_->new_referrer);
+  request_.referrer_policy = redirect_info_->new_referrer_policy;
+
+  redirect_info_.reset();
+
+  // This scheme may or may not be intercepted currently
+  MaybeRunInterceptHandler();
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::SetPriority(
+    net::RequestPriority priority,
+    int32_t intra_priority_value) {
+  // TODO - Maybe remove all of these is_bound checks? There's logic
+  // to reset the receiver if the target remote is disconnected, so
+  // in theory we shouldn't receive a message if the target is disconnected
+  if (target_loader_.is_bound()) {
+    target_loader_->SetPriority(priority, intra_priority_value);
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::
+    PauseReadingBodyFromNet() {
+  if (target_loader_.is_bound()) {
+    target_loader_->PauseReadingBodyFromNet();
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::
+    ResumeReadingBodyFromNet() {
+  if (target_loader_.is_bound()) {
+    target_loader_->ResumeReadingBodyFromNet();
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnReceiveEarlyHints(
+    network::mojom::EarlyHintsPtr early_hints) {
+  if (client_.is_bound()) {
+    client_->OnReceiveEarlyHints(std::move(early_hints));
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnReceiveResponse(
+    network::mojom::URLResponseHeadPtr head) {
+  if (client_.is_bound()) {
+    client_->OnReceiveResponse(std::move(head));
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnReceiveRedirect(
+    const net::RedirectInfo& redirect_info,
+    network::mojom::URLResponseHeadPtr head) {
+  // Save the redirect info so the request can be updated in |FollowRedirect|
+  redirect_info_ = redirect_info;
+  if (client_.is_bound()) {
+    client_->OnReceiveRedirect(redirect_info, std::move(head));
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnUploadProgress(
+    int64_t current_position,
+    int64_t total_size,
+    OnUploadProgressCallback callback) {
+  if (client_.is_bound()) {
+    client_->OnUploadProgress(current_position, total_size,
+                              std::move(callback));
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnReceiveCachedMetadata(
+    mojo_base::BigBuffer data) {
+  if (client_.is_bound()) {
+    client_->OnReceiveCachedMetadata(std::move(data));
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnTransferSizeUpdated(
+    int32_t transfer_size_diff) {
+  if (client_.is_bound()) {
+    client_->OnTransferSizeUpdated(transfer_size_diff);
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::
+    OnStartLoadingResponseBody(mojo::ScopedDataPipeConsumerHandle body) {
+  if (client_.is_bound()) {
+    client_->OnStartLoadingResponseBody(std::move(body));
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnComplete(
+    const network::URLLoaderCompletionStatus& status) {
+  if (client_.is_bound()) {
+    client_->OnComplete(status);
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::
+    MaybeRunInterceptHandler() {
+  // Check if user has intercepted this scheme.
+  auto it = intercepted_handlers_.find(request_.url.scheme());
+  if (it != intercepted_handlers_.end()) {
+    // Reset callback fired since this is a fresh invocation of the handler
+    callbackFired_ = false;
+
+    // <scheme, <type, handler>>
+    it->second.second.Run(
+        request_,
+        base::BindOnce(
+            &InterceptingURLLoaderFactory::InterceptedRequest::SendResponse,
+            base::Unretained(this), it->second.first),
+        base::BindOnce(&InterceptingURLLoaderFactory::InterceptedRequest::
+                           ContinueRequestFromHandler,
+                       base::Unretained(this)));
+  } else {
+    // Not intercepted, continue on to the target factory
+    ContinueRequest();
+  }
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::ContinueRequest() {
+  if (pending_follow_redirect_params_ && target_loader_.is_bound()) {
+    target_loader_->FollowRedirect(
+        pending_follow_redirect_params_->removed_headers,
+        pending_follow_redirect_params_->modified_headers,
+        pending_follow_redirect_params_->modified_cors_exempt_headers,
+        pending_follow_redirect_params_->new_url);
+  } else {
+    // Reset these if they're bound
+    target_loader_.reset();
+    client_receiver_.reset();
+
+    // Continue on to the target factory
+    target_factory_remote_->CreateLoaderAndStart(
+        target_loader_.BindNewPipeAndPassReceiver(), request_id_, options_,
+        request_, client_receiver_.BindNewPipeAndPassRemote(),
+        traffic_annotation_);
+  }
+
+  pending_follow_redirect_params_.reset();
+}
+
 void InterceptingURLLoaderFactory::InterceptedRequest::SendResponse(
+    ProtocolType type,
     gin::Arguments* args) {
   if (callbackFired_) {
     gin_helper::ErrorThrower(args->isolate())
@@ -52,13 +245,18 @@ void InterceptingURLLoaderFactory::InterceptedRequest::SendResponse(
 
   callbackFired_ = true;
 
-  ElectronURLLoaderFactory::StartLoading(std::move(loader_), request_id_,
-                                         options_, request_, std::move(client_),
-                                         traffic_annotation_, type_, args);
+  // Reset these if they're bound
+  target_loader_.reset();
+  client_receiver_.reset();
+
+  ElectronURLLoaderFactory::StartLoading(
+      target_loader_.BindNewPipeAndPassReceiver(), request_id_, options_,
+      request_, client_receiver_.BindNewPipeAndPassRemote(),
+      traffic_annotation_, type, args);
 }
 
-void InterceptingURLLoaderFactory::InterceptedRequest::ContinueRequest(
-    gin::Arguments* args) {
+void InterceptingURLLoaderFactory::InterceptedRequest::
+    ContinueRequestFromHandler(gin::Arguments* args) {
   if (callbackFired_) {
     gin_helper::ErrorThrower(args->isolate())
         .ThrowError("Response already sent for intercepted request");
@@ -66,13 +264,31 @@ void InterceptingURLLoaderFactory::InterceptedRequest::ContinueRequest(
   }
 
   callbackFired_ = true;
+  ContinueRequest();
+}
 
-  // Continue on to the target factory
-  mojo::Remote<network::mojom::URLLoaderFactory> target_factory_remote(
-      std::move(target_factory_));
-  target_factory_remote->CreateLoaderAndStart(
-      std::move(loader_), request_id_, options_, request_, std::move(client_),
-      traffic_annotation_);
+void InterceptingURLLoaderFactory::InterceptedRequest::OnClientDisconnect() {
+  client_.reset();
+  client_receiver_.reset();
+
+  MaybeDeleteThis();
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::OnLoaderDisconnect() {
+  target_loader_.reset();
+  loader_receiver_.reset();
+
+  MaybeDeleteThis();
+}
+
+void InterceptingURLLoaderFactory::InterceptedRequest::MaybeDeleteThis() {
+  // We can delete this factory when it is impossible for a new request to
+  // arrive in the future.
+  if (client_receiver_.is_bound() || loader_receiver_.is_bound()) {
+    return;
+  }
+
+  delete this;
 }
 
 InterceptingURLLoaderFactory::InterceptingURLLoaderFactory(
@@ -122,34 +338,15 @@ void InterceptingURLLoaderFactory::CreateLoaderAndStart(
     request.load_flags |= net::LOAD_IGNORE_LIMITS;
   }
 
-  // Check if user has intercepted this scheme.
-  auto it = intercepted_handlers_.find(request.url.scheme());
-  if (it != intercepted_handlers_.end()) {
-    mojo::PendingRemote<network::mojom::URLLoaderFactory> this_remote;
-    this->Clone(this_remote.InitWithNewPipeAndPassReceiver());
+  mojo::PendingRemote<network::mojom::URLLoaderFactory> target_factory_remote;
+  target_factory_remote_->Clone(
+      target_factory_remote.InitWithNewPipeAndPassReceiver());
 
-    mojo::PendingRemote<network::mojom::URLLoaderFactory> target_factory_remote;
-    target_factory_remote_->Clone(
-        target_factory_remote.InitWithNewPipeAndPassReceiver());
-
-    scoped_refptr<InterceptedRequest> intercepting_loader =
-        base::MakeRefCounted<InterceptedRequest>(
-            std::move(loader), request_id, options, request, std::move(client),
-            traffic_annotation, it->second.first, std::move(this_remote),
-            std::move(target_factory_remote));
-
-    // <scheme, <type, handler>>
-    it->second.second.Run(
-        request,
-        base::BindOnce(&InterceptedRequest::SendResponse, intercepting_loader),
-        base::BindOnce(&InterceptedRequest::ContinueRequest,
-                       intercepting_loader));
-  } else {
-    // Not intercepted, continue on to the target factory
-    target_factory_remote_->CreateLoaderAndStart(
-        std::move(loader), request_id, options, request, std::move(client),
-        traffic_annotation);
-  }
+  // Lifetime is tied to its receivers
+  // TODO - Make an InterceptedRequest::Create?
+  new InterceptedRequest(intercepted_handlers_, std::move(loader), request_id,
+                         options, request, std::move(client),
+                         traffic_annotation, std::move(target_factory_remote));
 }
 
 void InterceptingURLLoaderFactory::Clone(
@@ -158,6 +355,7 @@ void InterceptingURLLoaderFactory::Clone(
 }
 
 void InterceptingURLLoaderFactory::OnProxyingFactoryError() {
+  // TODO - Update this
   target_factory_remote_.reset();
   interceptor_receivers_.Clear();
 
@@ -165,6 +363,7 @@ void InterceptingURLLoaderFactory::OnProxyingFactoryError() {
 }
 
 void InterceptingURLLoaderFactory::OnInterceptorBindingError() {
+  // TODO - Update this
   if (interceptor_receivers_.empty())
     target_factory_remote_.reset();
 
@@ -172,6 +371,7 @@ void InterceptingURLLoaderFactory::OnInterceptorBindingError() {
 }
 
 void InterceptingURLLoaderFactory::MaybeDeleteThis() {
+  // TODO - Update this, only rely on receivers existing
   if (target_factory_remote_.is_bound() || !interceptor_receivers_.empty())
     return;
 
--- shell/browser/net/intercepting_url_loader_factory.h
+++ shell/browser/net/intercepting_url_loader_factory.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 GitHub, Inc.
+// Copyright (c) 2021 GitHub, Inc.
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
@@ -10,54 +10,106 @@
 #include <string>
 #include <vector>
 
-#include "base/memory/ref_counted.h"
 #include "content/public/browser/content_browser_client.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
 #include "shell/browser/net/electron_url_loader_factory.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace electron {
 
 class InterceptingURLLoaderFactory : public network::mojom::URLLoaderFactory {
  public:
-  class InterceptedRequest
-      : public base::RefCountedThreadSafe<InterceptedRequest> {
+  class InterceptedRequest : public network::mojom::URLLoader,
+                             public network::mojom::URLLoaderClient {
    public:
     InterceptedRequest(
-        mojo::PendingReceiver<network::mojom::URLLoader> loader,
+        const InterceptHandlersMap& intercepted_handlers,
+        mojo::PendingReceiver<network::mojom::URLLoader> loader_receiver,
         int32_t request_id,
         uint32_t options,
         const network::ResourceRequest& request,
         mojo::PendingRemote<network::mojom::URLLoaderClient> client,
         const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
-        ProtocolType type,
         mojo::PendingRemote<network::mojom::URLLoaderFactory>
-            interceptor_factory,
-        mojo::PendingRemote<network::mojom::URLLoaderFactory> target_factory);
+            target_factory_remote);
 
-    void SendResponse(gin::Arguments* args);
-    void ContinueRequest(gin::Arguments* args);
+    // network::mojom::URLLoader:
+    void FollowRedirect(
+        const std::vector<std::string>& removed_headers,
+        const net::HttpRequestHeaders& modified_headers,
+        const net::HttpRequestHeaders& modified_cors_exempt_headers,
+        const absl::optional<GURL>& new_url) override;
+    void SetPriority(net::RequestPriority priority,
+                     int32_t intra_priority_value) override;
+    void PauseReadingBodyFromNet() override;
+    void ResumeReadingBodyFromNet() override;
+
+    // network::mojom::URLLoaderClient:
+    void OnReceiveEarlyHints(
+        network::mojom::EarlyHintsPtr early_hints) override;
+    void OnReceiveResponse(network::mojom::URLResponseHeadPtr head) override;
+    void OnReceiveRedirect(const net::RedirectInfo& redirect_info,
+                           network::mojom::URLResponseHeadPtr head) override;
+    void OnUploadProgress(int64_t current_position,
+                          int64_t total_size,
+                          OnUploadProgressCallback callback) override;
+    void OnReceiveCachedMetadata(mojo_base::BigBuffer data) override;
+    void OnTransferSizeUpdated(int32_t transfer_size_diff) override;
+    void OnStartLoadingResponseBody(
+        mojo::ScopedDataPipeConsumerHandle body) override;
+    void OnComplete(const network::URLLoaderCompletionStatus& status) override;
+
+    void MaybeRunInterceptHandler();
+    void ContinueRequest();
+
+    void SendResponse(ProtocolType type, gin::Arguments* args);
+    void ContinueRequestFromHandler(gin::Arguments* args);
+
+    void OnClientDisconnect();
+    void OnLoaderDisconnect();
+    void MaybeDeleteThis();
 
    private:
-    ~InterceptedRequest();
+    ~InterceptedRequest() override;
     friend class base::RefCountedThreadSafe<InterceptedRequest>;
 
-    mojo::PendingReceiver<network::mojom::URLLoader> loader_;
+    const InterceptHandlersMap& intercepted_handlers_;
+
+    mojo::Receiver<network::mojom::URLLoader> loader_receiver_{this};
+    mojo::Receiver<network::mojom::URLLoaderClient> client_receiver_{this};
+
     int32_t request_id_;
     uint32_t options_;
     network::ResourceRequest request_;
-    mojo::PendingRemote<network::mojom::URLLoaderClient> client_;
     net::MutableNetworkTrafficAnnotationTag traffic_annotation_;
-    ProtocolType type_;
-    mojo::PendingRemote<network::mojom::URLLoaderFactory> interceptor_factory_;
-    mojo::PendingRemote<network::mojom::URLLoaderFactory> target_factory_;
+
+    mojo::Remote<network::mojom::URLLoaderClient> client_;
+    mojo::Remote<network::mojom::URLLoader> target_loader_;
+    mojo::Remote<network::mojom::URLLoaderFactory> target_factory_remote_;
 
     bool callbackFired_ = false;
+
+    absl::optional<net::RedirectInfo> redirect_info_;
+    // This stores the parameters to FollowRedirect that came from the client,
+    // so that they can be sent to |target_loader_| if request is continued.
+    struct FollowRedirectParams {
+      FollowRedirectParams();
+      ~FollowRedirectParams();
+      std::vector<std::string> removed_headers;
+      net::HttpRequestHeaders modified_headers;
+      net::HttpRequestHeaders modified_cors_exempt_headers;
+      absl::optional<GURL> new_url;
+
+      DISALLOW_COPY_AND_ASSIGN(FollowRedirectParams);
+    };
+    std::unique_ptr<FollowRedirectParams> pending_follow_redirect_params_;
 
     DISALLOW_COPY_AND_ASSIGN(InterceptedRequest);
   };
